e2f44bbde4067685000e15e2e295da1fee3f3ad6
yohannesluminari <y.luminari@gmail.com>
2024-06-18 17:57:43 +0200

    Fisrt commit user workiong not image impl

---
+++ UniMuisc/UniMuiscBackend/src/main/java/it/luminari/UniMuiscBackend/UniMuiscBackendApplication.java
@@ -0,0 +1,13 @@
+package it.luminari.UniMuiscBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniMuiscBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniMuiscBackendApplication.class, args);
+	}
+
+}
---
+++ UniMuisc/UniMuiscBackend/src/test/java/it/luminari/UniMuiscBackend/UniMuiscBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.luminari.UniMuiscBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniMuiscBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
