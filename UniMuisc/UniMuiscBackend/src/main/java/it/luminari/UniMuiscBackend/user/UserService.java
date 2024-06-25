1bfec8afd2137d9d23a9b7cafa5af5ca3467ae4f
yohannesluminari <y.luminari@gmail.com>
2024-06-25 16:15:51 +0200

    craezione regsiter e login manca encoder Password PASSWORD ENCODE AGGIUSTATO ORA FUZNIONA SIA DA RUNNER CHE DA POSTMAN

--- UniMuisc/UniMuiscBackend/src/main/java/it/luminari/UniMuiscBackend/user/UserService.java
+++ UniMuisc/UniMuiscBackend/src/main/java/it/luminari/UniMuiscBackend/user/UserService.java
@@ -5,6 +5,9 @@ import it.luminari.UniMuiscBackend.track.Track;
 import it.luminari.UniMuiscBackend.track.TrackRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +32,11 @@ public class UserService {
     @Autowired
     private JWTTools jwtTools;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String emailSender;
 
 
     public Response register(Request request) {
@@ -45,12 +53,42 @@ public class UserService {
 
         userRepository.save(user);
 
+        // Invia email di conferma registrazione all'utente
+        sendRegistrationConfirmationEmail(user);
+
+        // Invia email di notifica all'indirizzo y.luminari@gmail.com
+        sendAdminNotificationEmail(user);
+
         // Creazione della risposta senza includere la password
         Response response = new Response();
-        BeanUtils.copyProperties(user, response, "password"); // Escludi completamente la password
-
+        BeanUtils.copyProperties(user, response);
         return response;
     }
+
+    private void sendRegistrationConfirmationEmail(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmail());
+        message.setSubject("Benvenuto su UniMusic!");
+        message.setText("Grazie per esserti registrato su UniMusic. Goditi la tua permanenza!");
+
+        javaMailSender.send(message); // Utilizza javaMailSender invece di mailSender
+    }
+
+    private void sendAdminNotificationEmail(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("y.luminari@gmail.com");
+        message.setSubject("Nuova registrazione su UniMusic");
+        message.setText("L'utente " + user.getUsername() + " si è registrato con successo su UniMusic.");
+
+        javaMailSender.send(message); // Utilizza javaMailSender invece di mailSender
+    }
+
+
+
+
+
+
+
 
 
     public String login(Request request) {
